86a9dd27c7eed782030fe46f6f8f668984e7ab3d
wasabi2220 <99361862+wasabi2220@users.noreply.github.com>
2022-03-16 11:54:14 -0400

    Update IncomeTaxCalculator.py

--- IncomeTaxCalculator.py
+++ IncomeTaxCalculator.py
@@ -5,10 +5,12 @@ This is a Simple Income Tax Calculator Project for COP1047C Intro to Python
 MDC Project#1
 @supervisor: Eduardo Salcedo 
 @author: Willy
+revised: 03/16/2022 
+v1.1
 """
 
 #Initialize the constants value
-tax_rate=0.20 #Tax rate of 20% value
+tax_rate=(20/100) #Tax rate of 20% value
 standard_deduction=10000.00 #Standard deduction amount
 dependent_deduction=3000.00 #Dependent deduction amount
 
@@ -20,6 +22,6 @@ num_dependent=int(input("Please enter the number of dependents: ")) #Requesting 
 tax=float((income-standard_deduction-(num_dependent*dependent_deduction))*tax_rate) #Income Tax calculation in float
 
 #Print the result
-print(f"\nYour gross income taxis: ${income:.2f}") #Print the gross income input with two decimal to the screen
-print("\nYour number of dependets:",num_dependent) #Print the number of dependent to the screen
-print(f"\nThe income tax is ${tax:.2f}") #Print the total amount of income tax to the screen
+print(f"\nEnter the gross income tax: {income:.2f}") #Print the gross income input with two decimal to the screen
+print("\nENter the number of dependets:",num_dependent) #Print the number of dependent to the screen
+print(f"\nThe income tax is ${tax:.1f}") #Print the total amount of income tax to the screen
